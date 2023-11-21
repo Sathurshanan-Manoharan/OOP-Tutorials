80ee505e4390661833cdf3344031f767f8bf67fb
Sathurshanan-Manoharan <shathurshmanoharan@gmail.com>
2023-11-21 22:22:33 +0530

    Completed question four

---
+++ Tutorial-7/src/QuestionFour.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class QuestionFour {
+    public static void main(String[] args) {
+        boolean isExit = true;
+        ArrayList<Book> books = new ArrayList<>();
+        while(isExit){
+
+            Scanner input = new Scanner(System.in);
+            System.out.print("Enter Book title : ");
+            String title = input.next();
+            System.out.print("Enter Book price : ");
+            double price = input.nextDouble();
+            System.out.print("Enter the publication year : ");
+            int year = input.nextInt();
+            System.out.print("Enter the author : ");
+            String author = input.next();
+
+            books.add(new Book(title, price, year, author));
+
+            System.out.print("Press y to continue or n to exit :");
+            String exit = input.next();
+            if (exit.equals("n"))isExit = false;
+            else continue;
+
+            for (int i = 0; i < books.size(); i++) {
+                System.out.println(books.get(i).toString());
+            }
+        }
+
+
+
+    }
+}
