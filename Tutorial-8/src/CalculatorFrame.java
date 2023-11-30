97f087151100cf7e71feaf7e69090a1de23e2a88
Sathurshanan-Manoharan <shathurshmanoharan@gmail.com>
2023-11-30 23:03:38 +0530

    Updated files

---
+++ Tutorial-8/src/CalculatorFrame.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class CalculatorFrame extends JFrame {
+// Constructor to setup UI components public CalculatorFrame () {
+// set here the layout for different panels
+// here you need to implement your code
+//…
+    this.setBackground(Color c);
+}
