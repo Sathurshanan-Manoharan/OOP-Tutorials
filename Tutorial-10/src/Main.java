7f45154bda1193cbd55545c601a82f430dce941b
Sathurshanan-Manoharan <shathurshmanoharan@gmail.com>
2023-12-11 21:31:04 +0530

    Completed tuorial ten

--- Tutorial-10/src/Main.java
+++ Tutorial-10/src/Main.java
@@ -1,5 +1,28 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        /*
+        try{
+            int myArray[] = new int[5];
+            // trying to access element 5
+            System.out.println(myArray[5]);
+        }
+            catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("The element " + e.getMessage() + " does not exist!");
+        }
+
+         */
+        try {
+            Scanner input = new Scanner(System.in);
+            int value = 0;
+            System.out.println("Please enter an integer");
+            value = input.nextInt();
+            System.out.println("Value: " + value);
+        } catch (InputMismatchException e){
+            System.out.println("Invalid input " + e);
+        }
 
     }
 }
